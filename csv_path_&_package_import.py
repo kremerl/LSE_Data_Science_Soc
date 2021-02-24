438d9429781d14bbf3503bd7a3b17454b6ec00b6
kremerl <76967201+kremerl@users.noreply.github.com>
2021-02-24 15:38:12 +0000

    import and select restaurants
    - can copy pasta and insert your own filepath

--- csv_path_&_package_import.py
+++ csv_path_&_package_import.py
@@ -4,3 +4,12 @@ import numpy as np
 import pandas as pd
 import json
 import tqdm
+
+
+# import csv and convert to dataframe
+business_info = pd.read_csv("/Users/louiskremer/Desktop/github/yelp_data_local/business_info.csv", delimiter = ",", header = 0, dtype = str)
+business_info = pd.DataFrame(business_info)
+
+
+
+
--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,11 @@
 # clean your data in here
+
+# make dataframe easily viewable
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+business_info.head()
+
+# select only restaurants
+business_info = business_info[business_info["categories"].str.contains("Restaurants", na = False)]
