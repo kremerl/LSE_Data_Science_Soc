7379b060d9c0e335fb211c7ccf3952344f2e0360
kremerl <76967201+kremerl@users.noreply.github.com>
2021-03-06 17:12:47 +0000

    readded prototype...

--- louis_food_poisoning_prediction.py
+++ louis_food_poisoning_prediction.py
@@ -16,11 +16,9 @@ lv_poisoned2 = lv_poisoned2.loc[lv_poisoned2["stars"] < 3.0]
 lv_poisoned2.count()
 lv_poisoned2.head()
 
-# function that returns 1 if poisened and 0 otherwise (works by matching business_id of total dataset to business_id of poisened dataframe)
 
+# function that returns 1 if poisened and 0 otherwise (works by matching business_id of total dataset to business_id of poisened dataframe)
 def is_poisoned(a, b):
-    a = pd.array(a)
-    b = pd.array(b)
     new_variable = []
     for row in a:
         if row in b:
@@ -29,14 +27,21 @@ def is_poisoned(a, b):
             new_variable.append(0)
     return new_variable
 
+# works on prototype
+one = ["abc123", "bcd234", "cde345"]
+two = ["abc123", "xyz"]
+
+is_poisoned(one,two)
 
 
+#DOES NOT WORK, ONLY 0s in output :(((
 # apply fn to full datasets to create a variable that classifies restaurants depending on whether or not they have already caused food poisoning
 lv_nlp2["poisoned"] = lv_nlp2.apply
 
 business_info["poisoned"] = is_poisoned(business_info["business_id"], lv_nlp2["business_id"])
 
 business_info.groupby("poisoned").size()
+
 
 
 
