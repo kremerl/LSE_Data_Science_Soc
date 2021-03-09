e87140f32e83ab831dcdecc52b67d4f708219024
kremerl <76967201+kremerl@users.noreply.github.com>
2021-03-09 15:07:04 +0000

    classifier finished
    yes baby

--- louis_food_poisoning_prediction.py
+++ louis_food_poisoning_prediction.py
@@ -2,77 +2,48 @@
 
 from collections import Counter
 
+#subset to Las Vegas only
+business_info = business_info[business_info["city"] == "Las Vegas"]
+
 # subset reviews that experienced food poisoning, by matching words to their reviews
 words = ["poison", "diarrhea", "diarrhoea", "vomit", "puking", "puked", "threw up", "throw up", "salmonella"] # don't use "sick", as only 1/2 correlated with negative review
 lv_poisoned = lv_nlp[lv_nlp["text"].str.contains(("|".join(words)), case = False, na = False)]
 
-#copy
-lv_poisoned2 = lv_poisoned.copy() #for experimentation :)
-lv_poisoned2.count()
-lv_nlp2 = lv_nlp.copy()
-
 # subset only bad reviews (nobody would give a good review to a restaurant in which they were poisoned)
-lv_poisoned2["stars"] = pd.to_numeric(lv_poisoned2["stars"]) # REMOVE once dataset is cleaned
-lv_poisoned2 = lv_poisoned2.loc[lv_poisoned2["stars"] < 3.0]
+lv_poisoned["stars"] = pd.to_numeric(lv_poisoned["stars"]) # REMOVE once dataset is cleaned
+lv_poisoned = lv_poisoned.loc[lv_poisoned["stars"] < 3.0]
 
-lv_poisoned2.count()
-lv_poisoned2.head()
-
-
-# function that returns 1 if poisened and 0 otherwise (works by matching business_id of total dataset to business_id of poisened dataframe)
-def is_poisoned(a, b):
-    new_variable = []
-    for row in a:
-        if row in b:
-            new_variable.append(1) #append is like [row/i] in R (to iterate over outcome variable)
-        else:
-            new_variable.append(0)
-    return new_variable
-
-# prototype of fn
-one = ["abc123", "bcd234", "cde345"]
-two = ["xyz", "xyz2", "abc123", "abc123"]
-is_poisoned(one,two)
+lv_poisoned.count()
+lv_poisoned.head()
 
 # convert data to list (fn does not accept pandas data types)
 business_id = business_info["business_id"].values.tolist()
-nlp_id = lv_nlp["business_id"].values.tolist()
 
 # get unique values from poisoned_id, makes it substantially faster to compute
 poisoned_id = lv_poisoned["business_id"].values.tolist()
 poisoned_id = set(poisoned_id.copy())
 poisoned_id = list(poisoned_id.copy())
 
-# WORKS, BUT ONLY FOR BUSINESS_INFO
-# apply fn to full datasets to create a variable that classifies restaurants depending on whether or not they have already caused food poisoning
-# business_info["poisoned"] = is_poisoned(business_info["business_id"], lv_poisoned2["business_id"]) # DOES NOT WORK WITH PANDAS for some reason I don't understand :(
-business_info["poisoned"] = is_poisoned(business_id, poisoned_id)
+# now count of poisoned reviews per restaurant and total reviews per restaurant
+poisoned_counter = Counter(lv_poisoned["business_id"])
+total_counter = Counter(lv_nlp["business_id"])
 
-#subset to LV only
-business_info_lv = business_info[business_info["city"] == "Las Vegas"]
-business_info_lv
-
-business_info_lv.groupby("poisoned").size()
-
-# now count of poisoned reviews per restaurant
-lv_poisoned_counter = Counter(lv_poisoned["business_id"])
-
+# function
 def is_poisoned(a, b):
     poisoned_binary = []
     for row in a:
         if row in b:
-            poisoned_count = lv_poisoned_counter[row]
-            total_count = lv_nlp[row]
+            poisoned_count = poisoned_counter[row]
+            total_count = total_counter[row]
             poisoned_binary.append(poisoned_count / total_count)
         else:
             poisoned_binary.append(0)
     return poisoned_binary
 
-business_info["poisoned"] = is_poisoned(business_id, poisoned_id)
+# business_info["poisoned"] = is_poisoned(business_info["business_id"], lv_poisoned2["business_id"]) # DOES NOT WORK WITH PANDAS data input
+business_info["%_poisoned"] = is_poisoned(business_id, poisoned_id)
 
-
-
-
+print(business_info["%_poisoned"])
 
 
 
@@ -93,3 +64,19 @@ for x in z:
 
 pd.Series(z)
 
+
+#other stuff
+# function that returns 1 if poisened and 0 otherwise (works by matching business_id of total dataset to business_id of poisened dataframe)
+def is_poisoned(a, b):
+    new_variable = []
+    for row in a:
+        if row in b:
+            new_variable.append(1) #append is like [row/i] in R (to iterate over outcome variable)
+        else:
+            new_variable.append(0)
+    return new_variable
+
+# prototype of fn
+one = ["abc123", "bcd234", "cde345"]
+two = ["xyz", "xyz2", "abc123", "abc123"]
+is_poisoned(one,two)
